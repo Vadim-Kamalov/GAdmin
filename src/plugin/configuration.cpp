13e9a999e1dcb659c37243172fb38fee4f3dcafb
Dmitry Lyubimcev <dzone@danwin1210.de>
2025-01-16 10:49:29 +0300

    fix: types in configuration.cpp must not be at the same line with a function’s definition.
    Signed-off-by: Dmitry Lyubimcev <dzone@danwin1210.de>

--- src/plugin/configuration.cpp
+++ src/plugin/configuration.cpp
@@ -2,7 +2,8 @@
 #include "plugin/log.h"
 #include <fstream>
 
-void plugin::configuration_initializer::write(const std::filesystem::path& path, const nlohmann::json& json) {
+void
+plugin::configuration_initializer::write(const std::filesystem::path& path, const nlohmann::json& json) {
     if (std::ofstream file = std::ofstream(path)) {
         file << std::setw(4) << json << std::endl;
         return;
@@ -11,7 +12,8 @@ void plugin::configuration_initializer::write(const std::filesystem::path& path,
     log::fatal("failed to write JSON to \"{}\"", path.string());
 }
 
-nlohmann::json plugin::configuration_initializer::get(const std::filesystem::path& path) {
+nlohmann::json
+plugin::configuration_initializer::get(const std::filesystem::path& path) {
     std::ifstream file(path);
 
     if (!file) {
@@ -30,11 +32,13 @@ nlohmann::json plugin::configuration_initializer::get(const std::filesystem::pat
     return file_json;
 }
 
-void plugin::configuration_initializer::save() const {
+void
+plugin::configuration_initializer::save() const {
     write(configuration_file, json);
 }
 
-void plugin::configuration_initializer::save(std::chrono::milliseconds after) const {
+void
+plugin::configuration_initializer::save(std::chrono::milliseconds after) const {
     auto now = std::chrono::steady_clock::now();
     if (std::chrono::duration_cast<std::chrono::milliseconds>(now - last_time_saved) >= after) {
         last_time_saved = now;
