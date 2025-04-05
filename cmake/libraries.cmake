83e84f6205f04e4b51dcea01e937fe81def6b324
Dmitry Lyubimcev <dzone@danwin1210.de>
2025-04-05 13:00:57 +0300

    build: change kthook fethchcontent repo
    Signed-off-by: Dmitry Lyubimcev <dzone@danwin1210.de>

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(json   URL https://github.com/nlohmann/json/releases/download/v3.11.3/json.tar.xz)
 FetchContent_Declare(imgui  GIT_REPOSITORY https://github.com/ocornut/imgui.git)
-FetchContent_Declare(kthook GIT_REPOSITORY https://github.com/defaultzone/kthook.git)
+FetchContent_Declare(kthook GIT_REPOSITORY https://github.com/kin4stat/kthook.git)
 FetchContent_MakeAvailable(json kthook imgui)
 
 target_include_directories(${PROJECT_NAME} PUBLIC ${imgui_SOURCE_DIR}/ ${kthook_SOURCE_DIR}/include/)
@@ -17,4 +17,4 @@ target_sources(${PROJECT_NAME} PUBLIC
     ${imgui_SOURCE_DIR}/backends/imgui_impl_dx9.cpp
 )
 
-set(LIBRARIES_LINKAGE d3d9 d3dx9 gdi32 dwmapi kthook wininet dbghelp nlohmann_json::nlohmann_json)
+set(LIBRARIES_LINKAGE d3d9 d3dx9 kthook gdi32 dwmapi wininet dbghelp nlohmann_json::nlohmann_json)
